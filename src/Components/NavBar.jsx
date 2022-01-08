a508aeb8095cbc5eb2a48dfe36f737707cb18c07
Leandro <leandro.ortega@darwoft.com>
2022-01-08 16:27:32 -0300

    crea navBar y estilos, instala sass

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function NavBar() {
+    return (
+        <>
+            <div className="navbar">
+                <div className="logo">Logo</div>
+                <div className="links">
+                    <ul>
+                        <li> <a href=""> Home</a></li>
+                        <li> <a href=""> Promociones</a></li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    )
+}
