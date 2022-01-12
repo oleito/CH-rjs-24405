8648781eb4e7dce315b423d191f41237d656ac64
Leandro <leandro.ortega@darwoft.com>
2022-01-11 23:12:31 -0300

    Creacion del item counter

--- src/Components/ItemListContainer.jsx
+++ src/Components/ItemListContainer.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import ItemCount from "./ItemCount";
 
 export default function ItemListContainer(props) {
+  function onAdd() {
+    console.log("Item added");
+  }
   return (
     <>
       <h3> {props.greeting} </h3>
+      <ItemCount stock={0} initial={1} onAdd={onAdd} />
     </>
   );
 }
