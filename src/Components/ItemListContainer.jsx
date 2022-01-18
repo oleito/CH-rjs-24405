b21c2aa7d199e45cbb1e80583d699f34e13b826a
Leandro <leandro.ortega@darwoft.com>
2022-01-18 00:20:15 -0300

    Crea json y se lo pasa a itemlist

--- src/Components/ItemListContainer.jsx
+++ src/Components/ItemListContainer.jsx
@@ -6,11 +6,27 @@ export default function ItemListContainer(props) {
   function onAdd() {
     console.log("Item added");
   }
+  let ItemListJson = [
+    {
+      id: "mb-45",
+      title: "Titulo de prueba",
+      description: "Descripcion del producto",
+      price: 123.45,
+      pictureUrl: "https://via.placeholder.com/200x300",
+    },
+    {
+      id: "mb-48",
+      title: "Titulo de prueba 2",
+      description: "Descripcion del segundo producto",
+      price: 678.9,
+      pictureUrl: "https://via.placeholder.com/200x300",
+    },
+  ];
   return (
     <>
       <h3> {props.greeting} </h3>
       {/* <ItemCount stock={0} initial={1} onAdd={onAdd} /> */}
-      <ItemList />
+      <ItemList list={ItemListJson} />
     </>
   );
 }
