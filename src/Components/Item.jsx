15683053ee27a4b1e9ef2e32fdd9b895e53e1f23
Leandro <leandro.ortega@darwoft.com>
2022-01-18 00:46:12 -0300

    Muestro las propiedades de cada item.

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -4,6 +4,9 @@ export default function Item(props) {
   return (
     <>
       <h5>{props.item.title}</h5>
+      <div>{props.item.description}</div>
+      <div>{props.item.price}</div>
+      <div>{props.item.stock}</div>
     </>
   );
 }
